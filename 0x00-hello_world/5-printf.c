928be2923afa1a8f5cc00fa4976b58b978018265
voskpaul <paulriemii@gmail.com>
2023-02-16 09:35:52 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+vi 5-printf.c
+#include <stdio.h>
+
+/**
+*main - Entry point
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
