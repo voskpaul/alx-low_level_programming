ead126d7e266ca39b8b36bb771ec0f15a0db08ea
voskpaul <paulriemii@gmail.com>
2023-03-10 14:21:36 -0800

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,68 +1,57 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 /**
- * main - prints the min num of coins to make change for an amount of money
- *
- * @argc: argument count
- * @argv: argument vector
- * Return: int
+ * main - prints the minimum number of coins
+ * to make change for an amount of money
+ * @argc: n args
+ * @argv: arr args
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
-	unsigned int count = 0;
+	int value, c;
 
+	c = 0;
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	else if (atoi(argv[1]) < 0)
+	value = atoi(argv[1]);
+	if (value < 0)
 	{
 		printf("%d\n", 0);
 		return (0);
 	}
-	count = coin_count(count, atoi(argv[1]));
-	printf("%d\n", count);
+	if (value % 25 >= 0)
+	{
+		c += value / 25;
+		value = value % 25;
+	}
+	if (value % 10 >= 0)
+	{
+		c += value / 10;
+		value = value % 10;
+	}
+	if (value % 5 >= 0)
+	{
+		c += value / 5;
+		value = value % 5;
+	}
+	if (value % 2 >= 0)
+	{
+		c += value / 2;
+		value = value % 2;
+	}
+	if (value % 1 >= 0)
+	{
+		c += value / 1;
+	}
+	printf("%d\n", c);
 	return (0);
 }
 
-/**
- * coin_count - counts the min coins needed for change
- *
- * @count: int to count coins
- * @num: argv[1] changed to int
- * Return: int
- */
-unsigned int coin_count(unsigned int count, int num)
-{
-	unsigned int sum = 0;
 
-	while (!(sum + 25 > (unsigned int)num))
-	{
-		sum += 25;
-		count++;
-	}
-	while (!(sum + 10 > (unsigned int)num))
-	{
-		sum += 10;
-		count++;
-	}
-	while (!(sum + 5 > (unsigned int)num))
-	{
-		sum += 5;
-		count++;
-	}
-	while (!(sum + 2 > (unsigned int)num))
-	{
-		sum += 2;
-		count++;
-	}
-	while (!(sum + 1 > (unsigned int)num))
-	{
-		sum += 1;
-		count++;
-	}
-	return (count);
-}
